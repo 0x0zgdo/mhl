f34cef15686e90d9caa24cd5ad0a6ee4a59451d3
0x0zgdo <zgdotech@gmail.com>
2026-01-31 21:35:51 +0800

    added the hashmap for the known malware samples

--- cyclic/scanFile.java
+++ cyclic/scanFile.java
@@ -1,5 +1,8 @@
-//This is a public method inside the ScanEngine class
+//This is a public method inside the ScanEngine class. It takes the absolute path of the file and calculate its SHA-1 checksum.
+//Once the has is generated it compares it to the known malware samples that is also hashed.
 
+
+private static final HashMap<String, String> KNOWN_MALWARE_SAMPLES = MapsKt.hashMapOf(TuplesKt.to("eicar.com", "3395856ce81f2b7382dee72602f798b642f14140"), TuplesKt.to("eicar.com.txt", "3395856ce81f2b7382dee72602f798b642f14140"), TuplesKt.to("eicar_com.zip", "d27265074c9eac2e2122ed69294dbc4d7cce9141"), TuplesKt.to("eicarcom2.zip", "bec1b52d350d721c7e22a6d4bb0a92909893a3ae"));
 public final boolean scanFile(File file) {
             Intrinsics.checkNotNullParameter(file, "file");
             try {
